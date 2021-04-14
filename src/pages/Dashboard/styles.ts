e2dc7df8c16c0cf639bb58f32a0ebbdece019e82
Emerson Lucena <emersonlucenaoficial@gmail.com>
2021-04-14 19:56:58 -0300

    added routes and we(me haha) get started main page

---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,6 @@
+import style from 'styled-components';
+
+export const Title = style.h1`
+  font-size: 48px;
+  color: #3A3A3A;
+`;
---
+++ src/routes/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Dashboard from '../pages/Dashboard/index';
+import Repository from '../pages/Repository/index'
+
+//switch é responsável por fazer que apenas uma rota seja exibida
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={Dashboard} />
+    <Route path="/repository" component={Repository} />
+  </Switch>
+);
+
+export default Routes;
